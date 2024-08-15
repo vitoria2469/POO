c16b7bdab0292c0dc2dd08994f53a32e5d3988bd
vitoria2469 <113610328+vitoria2469@users.noreply.github.com>
2024-08-15 18:02:21 -0300

    Add files via upload

---
+++ Contador.java
@@ -0,0 +1,18 @@
+package edu.curso;
+
+public class Contador {
+
+	public static void main(String[] args) {
+		int c = 1;
+		while(true) {
+			c = c + 1; //c += 1;
+//			if (c >= 100) {
+//				c = 0;
+//			c = c >= 100 ? 0 : c;	
+//			}
+			
+			System.out.println(c);
+		}
+	}
+
+}
