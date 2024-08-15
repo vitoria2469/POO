198eecf369dfb229dbe96afabb666099d97a98ee
vitoria2469 <113610328+vitoria2469@users.noreply.github.com>
2024-08-15 17:36:28 -0300

    Add files via upload

---
+++ SomarNumeros.java
@@ -0,0 +1,16 @@
+package edu.curso;
+import java.util.Scanner;
+
+public class SomarNumeros {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Por Favor digite um numero: ");
+		double num1 = input.nextDouble();
+		System.out.println("Por Favor digite outro numero: ");
+		double num2 = input.nextDouble();
+		Double resultado = Double.valueOf(num1 + num2);
+		System.out.printf("Resultado: %6.2f", resultado);
+		input.close();
+	}
+
+}
