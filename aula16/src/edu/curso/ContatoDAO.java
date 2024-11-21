a6bdf0314833a746293b0ff311282371206088f9
vitoria2469 <113610328+vitoria2469@users.noreply.github.com>
2024-11-21 18:08:07 -0300

    Add files via upload

---
+++ aula16/src/edu/curso/ContatoDAO.java
@@ -0,0 +1,13 @@
+package edu.curso;
+
+import java.util.List;
+
+public interface ContatoDAO {
+
+    void inserir(Contato c) throws ContatoException;
+    void atualizar(Contato c) throws ContatoException;
+    void remover(Contato c) throws ContatoException;
+    List<Contato> pesquisarPorNome(String nome) throws ContatoException;
+    List<Contato> pesquisarTodos() throws ContatoException;
+    
+}
